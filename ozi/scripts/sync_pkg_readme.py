d0036fc4c91336fca41e9275e0ae1b3c6a8f7e67
rjdbcm <rjdbcm@mail.umkc.edu>
2023-08-19 14:36:22 -0500

    FIX: CWE-23

--- ozi/scripts/sync_pkg_readme.py
+++ ozi/scripts/sync_pkg_readme.py
@@ -1,3 +1,4 @@
+# noqa: INP001
 # Copyright 2023 Ross J. Duff MSc
 # The copyright holder licenses this file
 # to you under the Apache License, Version 2.0 (the
@@ -24,7 +25,7 @@ try:
 except ImportError:
     import tomli as toml  # noqa: F401
 # pylint: disable=consider-using-with
-source = pathlib.Path(os.environ.get("MESON_SOURCE_ROOT", ".."))
+source = pathlib.Path(os.environ.get('MESON_SOURCE_ROOT', os.path.realpath('..')))
 pkg_info_file = open('PKG-INFO', 'r', encoding='utf-8')
 pkg_info = email.message_from_file(pkg_info_file).get_payload()
 pkg_info_file.close()
@@ -64,9 +65,9 @@ DIFF3 = str().join(
 if DIFF1 != '':
     print(DIFF1)
     sys.exit(len(DIFF1))
-elif DIFF2 != "":
+elif DIFF2 != '':
     print(DIFF2)
     sys.exit(len(DIFF2))
-elif DIFF3 != "":
+elif DIFF3 != '':
     print(DIFF3)
     sys.exit(len(DIFF3))
