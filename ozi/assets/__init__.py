c7da0ec0c710593aa4ab14fb2cdbd5b7070a54dd
rjdbcm <rjdbcm@mail.umkc.edu>
2023-08-31 18:21:15 -0500

    :fire::bento:: Delete ``assets/structure``

---
+++ ozi/assets/__init__.py
@@ -0,0 +1,1 @@
+"""Asset files for python packaging."""
---
+++ ozi/assets/status.py
@@ -0,0 +1,10 @@
+"""PyPA Status Classifiers"""
+choices = [
+    '1 - Planning',
+    '2 - Pre-Alpha',
+    '3 - Alpha',
+    '4 - Beta',
+    '5 - Production/Stable',
+    '6 - Mature',
+    '7 - Inactive',
+]
