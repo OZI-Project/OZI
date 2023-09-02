dfc96a80a551c91867aaa6427a01b16f32931324
rjdbcm <rjdbcm@mail.umkc.edu>
2023-09-01 21:36:36 -0500

    :hammer:: Add ``ozi`` script entrypoint.

---
+++ ozi/__main__.py
@@ -0,0 +1,21 @@
+"""OZI - Python Project Packaging
+
+Quick-start:
+$ ozi-new
+
+Add/remove files:
+$ ozi-fix
+
+"""
+import argparse
+import sys
+from typing import NoReturn, Union
+
+
+parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
+parser.add_argument('-v', '--version', action='store_true')
+
+
+def main() -> Union[NoReturn, None]:
+    """Main ozi entrypoint."""
+    parser.parse_args()
---
+++ ozi/assets/structure.py
@@ -0,0 +1,18 @@
+"""Directory structure expected files"""
+
+
+root_files = [
+    'README.rst',
+    '.gitignore',
+    'pyproject.toml',
+    'meson.build',
+    'meson.options',
+    'LICENSE.txt',
+    'PKG-INFO'
+]
+source_files = [
+    'meson.build',
+    '__init__.py',
+    '__init__.pyi',
+    'py.typed',
+]
