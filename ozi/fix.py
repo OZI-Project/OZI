2a2a131b83c19d3e70a9b72147c506aaff6294cc
rjdbcm <rjdbcm@mail.umkc.edu>
2023-09-02 23:30:19 -0500

    :hammer:: Update ``ozi-new`` and ``ozi-fix`` scripts.

--- ozi/fix.py
+++ ozi/fix.py
@@ -1,18 +1,20 @@
 """ozi-fix: Project fix script that outputs a meson rewriter JSON array."""
 
 import argparse
+import json
 from pathlib import Path
 import re
 import sys
 from typing import NoReturn, Union
+from warnings import warn
 
 from pyparsing import Regex
 
 
 from .assets.structure import root_files, source_files
 
-parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
-parser.add_argument('target', type=str, help='target project directory')
+parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__, add_help=False)
+parser.add_argument('target', type=str, help='target OZI project directory')
 parser.add_argument(
     '-a',
     '--add',
@@ -29,6 +31,16 @@ parser.add_argument(
     default=['ozi.phony'],
     help='remove file or dir/ from project',
 )
+output = parser.add_argument_group('output')
+output.add_argument('-p', '--pretty', action='store_true', help='pretty print JSON output')
+helpers = parser.add_mutually_exclusive_group()
+helpers.add_argument('-h', '--help', action='help', help='show this help message and exit')
+helpers.add_argument(
+    '-m',
+    '--missing',
+    action='store_true',
+    help='print missing files and exit with code set to miss count',
+)
 
 
 def main() -> Union[NoReturn, None]:
@@ -42,7 +54,7 @@ def main() -> Union[NoReturn, None]:
         'operation': 'src_add',
         'sources': None,
         'subdir': '',
-        'target_type': ''
+        'target_type': '',
     }
     miss_count = 0
     if not project.target.exists():
@@ -60,26 +72,53 @@ def main() -> Union[NoReturn, None]:
     with project.target.joinpath('PKG-INFO').open() as f:
         line = [next(f) for _ in range(2)][1]
         name = Regex(
-            '^(Name: )([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9._-]*[A-Za-z0-9])$', as_match=True,
+            '^(Name: )([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9._-]*[A-Za-z0-9])$',
+            as_match=True,
         ).parse_string(line)[0][2]
         project.name = re.sub(r'[-_.]+', '-', name).lower()  # type: ignore
+
+    found_root_files = []
+    for file in root_files:
+        if not project.target.joinpath(file).exists():
+            if project.missing:
+                print(Path(file))
+            miss_count += 1
+            continue
+        found_root_files.append(project.target.joinpath(project.name, file))
+    extra_root_files = [x for x in project.target.glob('./*') if x.is_file()]
+    extra_root_files = list(set(extra_root_files + found_root_files))
+
+    found_source_files = []
     for file in source_files:
         if not project.target.joinpath(project.name, file).exists():
-            print(f'Missing REQUIRED OZI project file: {file}')
+            if project.missing:
+                print(Path(project.name, file))
             miss_count += 1
+            continue
+        found_source_files.append(project.target.joinpath(project.name, file))
+    if project.missing:
+        if any(project.add):
+            warn('--missing is set: Ignoring -a/--add arguments', SyntaxWarning)
+        if any(project.remove):
+            warn('--missing is set: Ignoring -r/--remove arguments', SyntaxWarning)
+        exit(miss_count)
+    extra_source_files = [
+        x for x in (project.target / project.name).glob('./*') if x.is_file()
+    ]
+    extra_source_files = list(set(extra_source_files + found_source_files))
 
     add_source_files = []
     for file in project.add:
-        if str(file).startswith(f'{project.name}/'):
+        if file.is_dir():
+            (project.target / file).mkdir()
+        elif file.is_file():
             add_source_files += [file]
     if len(add_source_files) > 0:
         args = src_add.copy()
-        args.update({
-            'id': 'source_files',
-            'sources': add_source_files})
+        args.update({'target': 'source_files', 'sources': add_source_files})
         rewriter.append(args)
-    print('total missing files: ', miss_count)
-    print(rewriter)
+    print(json.dumps(rewriter, indent=4))
+    print(extra_source_files)
 
 
 if __name__ == '__main__':
--- ozi/new.py
+++ ozi/new.py
@@ -1,19 +1,47 @@
-"""ozi-new: quick-start project creation script."""
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+"""quick-start OZI project creation script."""
 import argparse
 from datetime import datetime, timezone
+from difflib import get_close_matches
 from pathlib import Path
 import re
 import sys
 from typing import NoReturn, Union
 from urllib.parse import urlparse
+from warnings import warn
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from spdx_license_list import LICENSES
 from email_validator import validate_email, EmailNotValidError
 from pyparsing import Regex, ParseException
+from trove_classifiers import classifiers
 
-from .assets import status, topic
-
+ambiguous_licenses = [
+    'OSI Approved :: Academic Free License (AFL)',
+    'OSI Approved :: Apache Software License',
+    'OSI Approved :: Apple Public Source License',
+    'OSI Approved :: Artistic License',
+    'OSI Approved :: BSD License',
+    'OSI Approved :: GNU Affero General Public License v3',
+    'OSI Approved :: GNU Free Documentation License (FDL)',
+    'OSI Approved :: GNU General Public License (GPL)',
+    'OSI Approved :: GNU General Public License v2 (GPLv2)',
+    'OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+    'OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+    'OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+]
+top4 = [
+    'MIT License',
+    'BSD License',
+    'GNU General Public License v3',
+    'Apache Software License',
+]
+status_prefix = 'Development Status :: '
+license_prefix = 'License :: '
+topic_prefix = 'Topic :: '
 root_templates = [
     '.gitignore',
     'meson.build',
@@ -27,95 +55,220 @@ source_templates = [
     'project.name/__init__.pyi',
     'project.name/meson.build',
 ]
+status = [
+    i[len(status_prefix):].lstrip() for i in classifiers if i.startswith(status_prefix)
+]
+licenses = [
+    i[len(license_prefix):].lstrip() for i in classifiers if i.startswith(license_prefix)
+]
+topic = [i[len(topic_prefix):].lstrip() for i in classifiers if i.startswith(topic_prefix)]
+license_spdx = [k for k, v in LICENSES.items() if v.deprecated_id is False]
+
+
+class CloseMatch(argparse.Action):
+    """Special choices action. Warn the user if a close match could not be found."""
+
+    license = licenses
+    status = status
+    topic = topic
+    license_spdx = license_spdx
+
+    def __init__(
+        self: argparse.Action,
+        option_strings,  # noqa: ANN001
+        dest,  # noqa: ANN001
+        nargs=None,  # noqa: ANN001
+        **kwargs,  # noqa: ANN003
+    ) -> None:
+        """argparse init"""
+        if nargs is not None:
+            raise ValueError('nargs not allowed')
+
+        super().__init__(option_strings, dest, **kwargs)
+
+    def __call__(
+        self: argparse.Action,
+        parser: argparse.ArgumentParser,
+        namespace: argparse.Namespace,
+        values: str,
+        option_string: str,
+    ) -> None:
+        """Action business logic."""
+        key = option_string.lstrip('-').replace('-', '_')
+        try:
+            values = get_close_matches(values, self.__getattribute__(key), cutoff=0.40)[0]
+        except IndexError:
+            warn(
+                '\n'.join(
+                    [
+                        f'No {key} choice matching "{values}" available.',
+                        'To list available options:',
+                        f'$ ozi-new -l {key}',
+                    ]
+                ),
+                RuntimeWarning,
+            )
+        setattr(namespace, self.dest, values)
+
+
 env = Environment(
     loader=FileSystemLoader(['templates', 'templates/project.name']),
     autoescape=select_autoescape(),
     enable_async=True,
 )
-parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
-parser.add_argument(
-    '--no-verify-email', action='store_true', help='skip email domain deliverability check'
+parser = argparse.ArgumentParser(
+    prog='ozi-new', description=sys.modules[__name__].__doc__, add_help=False
 )
-parser.add_argument('--name', type=str, help='name of project', required=True)
-parser.add_argument('--author', type=str, help='author of project', required=True)
-parser.add_argument('--email', type=str, help='valid author email', required=True)
-parser.add_argument('--summary', type=str, help='short summary', required=True)
-parser.add_argument('--homepage', type=str, help='homepage URL', required=True)
-parser.add_argument(
-    'target', type=str, help='target directory for new project',
-)
-parser.add_argument(
+subparser = parser.add_subparsers(help='project help')
+project_parser = subparser.add_parser('project', add_help=False)
+required = project_parser.add_argument_group('required')
+required.add_argument('--name', type=str, help='name of project')
+required.add_argument('--author', type=str, help='author of project')
+required.add_argument('--email', type=str, help='valid author email')
+required.add_argument('--summary', type=str, help='short summary')
+required.add_argument('--homepage', type=str, help='homepage URL')
+required.add_argument(
     '--license-spdx',
     type=str,
-    choices=[k for k, v in LICENSES.items() if v.deprecated_id is False],
-    metavar='ID',
+    metavar='ID e.g. MIT, BSD-2-Clause, GPL-3.0-or-later, Apache-2.0',
     help='SPDX short ID',
 )
-parser.add_argument(
-    '--license', type=str, help='license classifier (Pending PEP 639 Deprecation)'
+required.add_argument(
+    '--license',
+    type=str,
+    metavar=f'LICENSE e.g. {", ".join(top4)}',
+    help='license classifier',
+    action=CloseMatch,
 )
-parser.add_argument(
+required.add_argument(
+    'target',
+    type=str,
+    help='target directory for new project',
+)
+email = project_parser.add_argument_group('email options')
+email.add_argument(
+    '--verify-email',
+    default='--verify-email',
+    action=argparse.BooleanOptionalAction,
+    help='email domain deliverability check',
+)
+defaults = project_parser.add_argument_group('defaults')
+defaults.add_argument(
     '--copyright-head',
     type=str,
     default='',
     help='copyright header string',
-    metavar='COPYRIGHT_HEAD="Copyright {year}, {project.author}\\nSee LICENSE..."',
+    metavar='COPYRIGHT_HEAD="Copyright {year}, {author}\\nSee LICENSE..."',
 )
-parser.add_argument(
+defaults.add_argument(
+    '--ci-provider',
+    type=str,
+    default='github',
+    choices=('github',),
+    help='continuous integration and release provider',
+)
+project_output = project_parser.add_mutually_exclusive_group()
+project_output.add_argument(
+    '-h', '--help', action='help', help='show this help message and exit'
+)
+
+defaults.add_argument(
     '--topic',
-    choices=topic.choices,
+    choices=topic,
     default='Utilities',
     help='Python package topic',
     metavar='TOPIC="Utilities"',
     type=str,
 )
-parser.add_argument(
+defaults.add_argument(
     '--status',
-    choices=status.choices,
+    choices=status,
     default='1 - Planning',
     help='Python package status',
     metavar='STATUS="1 - Planning"',
     type=str,
+)
+output = parser.add_mutually_exclusive_group()
+output.add_argument('-h', '--help', action='help', help='show this help message and exit')
+output.add_argument(
+    '-l',
+    '--list',
+    type=str,
+    choices=['license', 'license-spdx', 'status', 'topic'],
+    help='list valid option settings and exit',
 )
 
 
 def main() -> Union[NoReturn, None]:
     """Main ozi.new entrypoint."""
     project = parser.parse_args()
+    if project.list == 'license':
+        print(*sorted((i for i in licenses)), sep='\n')
+        exit(0)
+    if project.list == 'license-spdx':
+        print(
+            *sorted((k for k, v in LICENSES.items() if v.deprecated_id is False)),
+            sep='\n'
+        )
+        exit(0)
+    if project.list == 'status':
+        print(*sorted((i for i in status)), sep='\n')
+        exit(0)
+    if project.list == 'topic':
+        print(*sorted(i for i in topic), sep='\n')
+        exit(0)
 
+    if 'project' not in project:
+        parser.print_help()
+        exit(0)
+
+    year = datetime.now(tz=datetime.now(timezone.utc).astimezone().tzinfo).year
     if len(project.copyright_head) == 0:
-        copyright_year = datetime.now(tz=datetime.now(timezone.utc).astimezone().tzinfo).year
         project.copyright_head = '\n'.join(
             [
-                f'Copyright {copyright_year}, {project.author}',
+                f'Copyright {year}, {project.author}',
                 'See LICENSE.txt in the project root for details.',
             ]
         )
+    else:
+        project.copyright_head = project.copyright_head.format(
+            year=year, author=project.author
+        )
+
+    if project.license in ambiguous_licenses:
+        msg = [
+            f'Ambiguous License string per PEP 639: {project.licenses}',
+            'See also: https://github.com/pypa/trove-classifiers/issues/17',
+            'This will need updated when PEP 639 is implemented.',
+        ]
+        warn('\n'.join(msg), PendingDeprecationWarning)
 
     if len(project.summary) > 512:
-        raise ValueError('Project summary exceeds 512 characters in length.')
+        warn('Project summary exceeds 512 characters (PyPI limit).', RuntimeWarning)
 
     try:
-        emailinfo = validate_email(project.email, check_deliverability=(~project.no_verify_email))
+        emailinfo = validate_email(
+            project.email, check_deliverability=(~project.no_verify_email)
+        )
+        project.email = emailinfo.normalized
     except EmailNotValidError as e:
-        raise ValueError(str(e), 'Invalid maintainer email format or domain unreachable.')
+        warn(f'{str(e)}\nInvalid maintainer email format or domain unreachable.', RuntimeWarning)
 
     try:
         Regex('^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$', re.IGNORECASE).parse_string(
             project.name
         )
     except ParseException as e:
-        raise ValueError(str(e), 'Invalid project name.')
+        warn(f'{str(e)}\nInvalid project name.', RuntimeWarning)
 
     home_url = urlparse(project.homepage).geturl
     if home_url.scheme != 'https':
-        raise ValueError('Homepage url scheme unsupported.')
+        warn('Homepage url scheme unsupported.', RuntimeWarning)
 
     if home_url.netloc == '':
-        raise ValueError('Homepage url netloc cound not be parsed.')
+        warn('Homepage url netloc cound not be parsed.', RuntimeWarning)
 
     project.name = re.sub(r'[-_.]+', '-', project.name).lower()
-    project.email = emailinfo.normalized
     project.target = Path(project.target)
 
     env.globals = {'project': vars(project)}
