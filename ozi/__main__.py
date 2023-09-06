d804b320d7d074f9085b8b31631c2f614b8695c0
rjdbcm <rjdbcm@mail.umkc.edu>
2023-09-05 22:27:40 -0500

    :hammer:: Improve main entrypoint help.
    Also made ``--version`` work.

--- ozi/__main__.py
+++ ozi/__main__.py
@@ -9,11 +9,26 @@ $ ozi-fix
 """
 import argparse
 import sys
+from importlib_metadata import version
 from typing import NoReturn, Union
 
 
-parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
-parser.add_argument('-v', '--version', action='store_true')
+def print_version() -> None:
+    """print current version string"""
+    print(version('ozi'))
+    exit(0)
+
+
+parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__, add_help=False)
+helpers = parser.add_mutually_exclusive_group()
+helpers.add_argument('-h', '--help', action='help', help='show this help message and exit')
+helpers.add_argument(
+    '-v',
+    '--version',
+    action='store_const',
+    const=print_version,
+    help='print out the current version and exit',
+)
 
 
 def main() -> Union[NoReturn, None]:
