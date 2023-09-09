b6362b454858aeb04dc2930a5c06a42088c6bc3a
rjdbcm <rjdbcm@mail.umkc.edu>
2023-09-09 17:56:50 -0500

    :bug::hammer:: Fix incorrect use of split function.

--- ozi/new.py
+++ ozi/new.py
@@ -272,7 +272,7 @@ def main() -> Union[NoReturn, None]:
     if any(project.target.iterdir()):
         raise FileExistsError('Directory not empty.')
 
-    env.globals = env.globals | {'project': vars(project), 'split': str.split}
+    env.globals = env.globals | {'project': vars(project)}
     Path(project.target, project.name).mkdir()
     Path(project.target, '.github', 'workflows').mkdir(parents=True)
     Path(project.target, 'subprojects').mkdir()
