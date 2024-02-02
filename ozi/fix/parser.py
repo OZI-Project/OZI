692d8bdf2822b86e0428d77ab18de7d2b3c0f110
rjdbcm <rjdbcm@mail.umkc.edu>
2024-02-02 17:57:18 -0600

    :pencil2: fix missing import of argparse. Signed-off-by: Eden Rose Duff MSc

--- ozi/fix/parser.py
+++ ozi/fix/parser.py
@@ -2,6 +2,7 @@
 # Part of the OZI Project, under the Apache License v2.0 with LLVM Exceptions.
 # See LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+import argparse
 import sys
 from argparse import SUPPRESS
 from argparse import ArgumentParser
