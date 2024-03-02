962f91b7da73d076d6a858b672a7d8fc14003729
rjdbcm <rjdbcm@mail.umkc.edu>
2024-03-02 13:57:13 -0600

    :hammer: Add pep-723 metadata.
    Signed-off-by: Eden Rose Duff MSc <help@oziproject.dev>

--- ozi/scripts/replace_ruff_target_version.py
+++ ozi/scripts/replace_ruff_target_version.py
@@ -3,6 +3,9 @@
 # Part of the OZI Project, under the Apache License v2.0 with LLVM Exceptions.
 # See LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# /// script
+# requires-python = ">=3.10"
+# ///
 """:pep:`723` script: replace commented target-version in [tool.ruff].
 
 Side-effects
