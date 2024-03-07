9b187e3601014347bcc5999001f1f9f769b6826d
Eden Rose, MSc <rjdbcm@outlook.com>
2024-03-06 21:46:56 -0600

    * :hammer: Add render_requirements.py script.
    * :hammer: Add render_requirements.py script.
    
    This strips comments from requirements.in
    
    Signed-off-by: rjdbcm <rjdbcm@mail.umkc.edu>

---
+++ ozi/scripts/render_requirements.py
@@ -0,0 +1,34 @@
+# noqa: INP001
+# ozi/scripts/render_requirements.py
+# Part of the OZI Project, under the Apache License v2.0 with LLVM Exceptions.
+# See LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# /// script
+# requires-python = ">=3.10"
+# ///
+""":pep:`723` script: render OZI-style ``requirements.in`` as ``PKG-INFO`` headers.
+
+Output
+^^^^^^
+
+* ``Requires-Dist`` headers for the :file:`{PWD}/requirements.in`
+
+Environment Variables
+^^^^^^^^^^^^^^^^^^^^^
+
+* :envvar:`PWD`
+
+
+"""
+import pathlib
+
+if __name__ == '__main__':
+    requirements = (
+        r.partition('\u0023')[0]
+        for r in filter(
+            lambda r: not (r.startswith('\u0023') or r == '\n'),
+            pathlib.Path('./requirements.in').read_text('utf-8').splitlines(),
+        )
+    )
+    for req in requirements:
+        print(f'Requires-Dist: {req}')
