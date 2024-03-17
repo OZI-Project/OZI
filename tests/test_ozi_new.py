33b3a2536bdbedb8f4fa1ab127ca5105aaccd2a6
rjdbcm <rjdbcm@mail.umkc.edu>
2024-03-16 22:29:02 -0500

    Fix fuzzer inputs.
    Signed-off-by: rjdbcm <rjdbcm@mail.umkc.edu>

--- tests/test_ozi_new.py
+++ tests/test_ozi_new.py
@@ -74,13 +74,17 @@ from ozi.tap import TAP  # pyright: ignore
             'license_exception_id': st.one_of(
                 list(map(st.just, ozi.actions.ExactMatch().license_exception_id)),  # type: ignore
             ),
-            'topic': st.one_of(list(map(st.just, ozi.actions.ExactMatch().topic))),  # type: ignore
-            'audience': st.one_of(list(map(st.just, ozi.actions.ExactMatch().audience))),  # type: ignore
-            'framework': st.one_of(list(map(st.just, ozi.actions.ExactMatch().framework))),  # type: ignore
-            'environment': st.one_of(
-                list(map(st.just, ozi.actions.ExactMatch().environment)),  # type: ignore
+            'topic': st.lists(st.one_of(list(map(st.just, ozi.actions.ExactMatch().topic)))),  # type: ignore
+            'audience': st.lists(st.one_of(list(map(st.just, ozi.actions.ExactMatch().audience)))),  # type: ignore
+            'framework': st.lists(
+                st.one_of(list(map(st.just, ozi.actions.ExactMatch().framework))),  # type: ignore
             ),
-            'status': st.one_of(list(map(st.just, ozi.actions.ExactMatch().status))),  # type: ignore
+            'environment': st.lists(
+                st.one_of(
+                    list(map(st.just, ozi.actions.ExactMatch().environment)),  # type: ignore
+                ),
+            ),
+            'status': st.lists(st.one_of(list(map(st.just, ozi.actions.ExactMatch().status)))),  # type: ignore
             'dist_requires': st.lists(
                 st.from_regex(
                     r'^([A-Za-z]|[A-Za-z][A-Za-z0-9._-]*[A-Za-z0-9]){1,80}$',
