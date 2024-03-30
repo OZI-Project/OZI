417ee261477f34b7039ba22e487b3ccff08bce4a
rjdbcm <rjdbcm@mail.umkc.edu>
2024-03-29 21:17:30 -0500

    :bug::pushpin: Fix Spec should pin workflow versions #103
    Signed-off-by: rjdbcm <rjdbcm@mail.umkc.edu>

--- ozi/spec/__init__.py
+++ ozi/spec/__init__.py
@@ -3,9 +3,15 @@
 # See LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 """Specification API for OZI Metadata."""
+from ozi.spec._spec import OZI
 from ozi.spec._spec import Metadata
 from ozi.spec._spec import current_version
 
-__all__ = ('current_version', 'METADATA')
+__all__ = (
+    'METADATA',
+    'Metadata',
+    'OZI',
+    'current_version',
+)
 
 METADATA = Metadata()
