3551fc9fa1752a03d87dc8063f583c74713b8e1c
rjdbcm <rjdbcm@mail.umkc.edu>
2024-04-10 09:48:33 -0500

    :rotating_light::pencil2: Linted with black 2024
    Signed-off-by: rjdbcm <rjdbcm@mail.umkc.edu>

--- ozi/spec/base.py
+++ ozi/spec/base.py
@@ -36,11 +36,9 @@ class _FactoryDataclass(Protocol):
 
     __dataclass_fields__: ClassVar[dict[str, _VT]]
 
-    def asdict(self: Self) -> dict[str, _VT]:
-        ...
+    def asdict(self: Self) -> dict[str, _VT]: ...
 
-    def __call__(self: Self) -> _FactoryMethod:
-        ...
+    def __call__(self: Self) -> _FactoryMethod: ...
 
 
 @dataclass(frozen=True)
