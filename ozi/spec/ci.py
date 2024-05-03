c18b76a4392c7497c61c40013ca10ee2dd9159cf
Eden Rose, MSc <ozi.project@outlook.com>
2024-05-02 19:48:09 -0500

    Update ci.py - release 0.2.1
    Signed-off-by: Eden Rose, MSc <ozi.project@outlook.com>

--- ozi/spec/ci.py
+++ ozi/spec/ci.py
@@ -23,7 +23,7 @@ class Publish(Default):
 class Release(Default):
     """Release patterns for packaged project."""
 
-    version: str = '0.2.0'
+    version: str = '0.2.1'
 
 
 @dataclass(slots=True, frozen=True, eq=True)
