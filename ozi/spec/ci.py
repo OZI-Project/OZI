7d1b5f3e690494368407c1ef5defff5aca8a68fb
rjdbcm <ozi.project@outlook.com>
2024-06-06 20:43:36 -0500

    :pushpin: OZI-Project/draft 0.3.1
    Signed-off-by: rjdbcm <ozi.project@outlook.com>

--- ozi/spec/ci.py
+++ ozi/spec/ci.py
@@ -24,7 +24,7 @@ class Publish(Default):
 class Draft(Default):
     """Draft release patterns for packaged project."""
 
-    version: str = '0.3.0'
+    version: str = '0.3.1'
 
 
 @dataclass(slots=True, frozen=True, eq=True)
