10fb6dad4fa4b652842331ad4748379e9fe8aba2
Eden Ross Duff, MSc <ozi.project@outlook.com>
2025-05-26 17:21:07 -0500

    :wrench: update cflite build.sh to use OZI wheel build
    Signed-off-by: Eden Ross Duff, MSc <ozi.project@outlook.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,7 @@
 # Build and install project (using current CFLAGS, CXXFLAGS). This is required
 # for projects with C extensions so that they're built with the proper flags.
-pip3 install .
+tox -e invoke -- release
+pip install dist/*.whl
 
 # Build fuzzers into $OUT. These could be detected in other ways.
 for fuzzer in $(find $SRC -name 'test_*.py'); do
